e2e946c65c780b0dc77d1b1af6ba5cc90314e4fb
hiroto <kitta1213@gmail.com>
2025-05-25 16:04:15 +0900

    Prismaサービスの実装

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
+/**
+ * PrismaServiceはNestJSのサービスとして定義されています。
+ * このサービスはPrismaClientを継承し、NestJSのライフサイクルイベントを利用して
+ * データベース接続を管理します。
+ */
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  async onModuleInit() {
+    // NestJSアプリケーション起動時にデータベースに接続
+    await this.$connect();
+  }
+}
